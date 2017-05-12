aef5e78e4e06afa0c5841af8a64e75d5a98add33
igorosha53 <igorosha@gmail.com>
2017-05-12 20:07:36 +0300

    my first commit

--- igor/src/main/java/Test.java
+++ igor/src/main/java/Test.java
@@ -1,2 +1,7 @@
 public class Test {
+    public static void main(String[] args) {
+        System.out.print("Hello");
+        System.out.print(", ");
+        System.out.print("World!!!");
+    }
 }
