1b82501398a77440af43d086ae94ca1e86ecf273
Igor <igorosha@gmail.com>
2017-05-13 14:18:08 +0300

    copy to notebook

---
+++ igor/src/main/java/lessons/lesson_2/Test_hw01.java
@@ -0,0 +1,12 @@
+package lessons.lesson_2;
+
+/**
+ * Created by User on 13.05.2017.
+ */
+public class Test_hw01 {
+    public static void main(String[] args) {
+        byte b1 = 2;
+        byte b2 = 4;
+        byte b3 = (byte) (b1 + b2);
+    }
+}
