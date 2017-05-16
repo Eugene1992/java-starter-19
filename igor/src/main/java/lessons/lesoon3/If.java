63861137e91dd85aa817f6bf524e0c7e4846f5b0
igorosha53 <igorosha@gmail.com>
2017-05-16 17:07:35 +0300

    my test variable

---
+++ igor/src/main/java/lessons/lesoon3/If.java
@@ -0,0 +1,15 @@
+package lessons.lesoon3;
+
+/**
+ * Created by User on 5/15/2017.
+ */
+public class If {
+    public static void main(String[] args) {
+        int k = 9;
+        int j;
+
+        if (k >= 0) {
+
+        }
+    }
+}
--- igor/src/main/java/lessons/lesson_2/Hw_06.java
+++ igor/src/main/java/lessons/lesson_2/Hw_06.java
@@ -8,5 +8,43 @@ public class Hw_06 {
         byte b2 = 4;
         byte b3 = (byte) (b1 + b2);
         System.out.println(b3);
+        System.out.println();
+
+
+        int l = 4;
+        int j = 9;
+        j = j + j;
+        j = l + l;
+        System.out.println("l= " + l);
+        System.out.println("j= " + j);
+        System.out.println();
+
+        int a = 2;
+        int b = a;
+        b = a + b;
+        a = b + a;
+
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println();
+
+        String s = "Hello ";
+        String po = "World";
+        s = po;
+        System.out.println(po);
+        System.out.println(s);
+        System.out.println(s);
+
+        int h = 15;
+        h = h + h;
+        System.out.println(h);
+        System.out.println();
+
+        byte u = (byte) 129;
+        u = (byte) (u + u);
+        System.out.println(u);
+        System.out.println();
+
+        System.out.println("nvnvn " + (10 > 7));
     }
 }
