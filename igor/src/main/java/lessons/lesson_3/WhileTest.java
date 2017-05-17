e051cccc9d8d3247637001b96983fb1156639a8d
Igor <igorosha@gmail.com>
2017-05-17 19:57:25 +0300

    while testing

---
+++ igor/src/main/java/lessons/lesson_3/WhileTest.java
@@ -0,0 +1,14 @@
+package lessons.lesson_3;
+
+/**
+ * Created by User on 17.05.2017.
+ */
+public class WhileTest {
+    public static void main(String[] args) {
+        int count = 1;
+        while (count < 100) {
+            System.out.println("count is: " + count);
+            count += 2;
+        }
+    }
+}
