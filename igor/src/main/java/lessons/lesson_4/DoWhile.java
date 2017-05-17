ba68626a3b04e29c64847447e1358c33bcdc85c7
Igor <igorosha@gmail.com>
2017-05-17 20:15:21 +0300

    do while testing

---
+++ igor/src/main/java/lessons/lesson_4/DoWhile.java
@@ -0,0 +1,16 @@
+package lessons.lesson_4;
+
+/**
+ * Created by User on 17.05.2017.
+ */
+public class DoWhile {
+    public static void main(String[] args) {
+        int count = 100;
+        do {
+            System.out.println(count);
+            count--;
+
+        } while (count < 100);
+
+    }
+}
