b94cfa5fc7ae2d1505a1dedaf6de1da5e7eb2d26
Scorpion96 <Nikitos96@ukr.net>
2017-05-18 23:52:21 +0300

    HW01. First - 1 task. Second - 2,3,4,5. Third - 6. Fourth - 7. Fifth - 8. Sixth - 9. Seventh - 10.

--- nikita/src/main/java/hw02/Task04.java
+++ nikita/src/main/java/hw02/Task04.java
@@ -12,16 +12,30 @@ public class Task04 {
         double b = scan.nextDouble();
         double c = scan.nextDouble();
         double d = scan.nextDouble();
-
-        if (a > b && a > c && a > d) {
-            System.out.println(b);
-        } else if (b > c && b > d) {
+        if (a > b && a > d && a < c) {
             System.out.println(a);
-        } else if (d > c) {
+        } else if (a > b && a > c && a < d) {
+            System.out.println(a);
+        } else if (a > d && a > c && a < b) {
+            System.out.println(a);
+        } else if (b > a && b > d && b < c) {
+            System.out.println(b);
+        } else if (b > a && b > c && b > d) {
+            System.out.println(b);
+        } else if (b > d && b > c && b < a) {
+            System.out.println(b);
+        } else if (c > d && c > b && c < a){
             System.out.println(c);
-        } else {
+        } else if (c > b && c > a && c < d) {
+            System.out.println(c);
+        } else if (c > a && c > d && c < b) {
+            System.out.println(c);
+        } else if (d > a && d > b && d < c){
+            System.out.println(d);
+        } else if (d > a && d > c && d < b) {
+            System.out.println(d);
+        } else if (d > b && d > c && d < a) {
             System.out.println(d);
         }
-
     }
 }
