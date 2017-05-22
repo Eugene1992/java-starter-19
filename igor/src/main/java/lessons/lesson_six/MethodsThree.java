1406db3a1983f815efe9e1e55b41269da2560162
igorosha53 <igorosha@gmail.com>
2017-05-22 21:16:58 +0300

    in lesson six add ex.3

---
+++ igor/src/main/java/lessons/lesson_six/MethodsThree.java
@@ -0,0 +1,17 @@
+package lessons.lesson_six;
+
+/**
+ * Created by User on 5/22/2017.
+ */
+public class MethodsThree {
+    public static void main(String[] args) {
+
+        int h = numberS(8, 9);
+        System.out.println(h);
+    }
+
+    public static int numberS(int i, int i1) {
+        int res = i + i1;
+        return res;
+    }
+}
