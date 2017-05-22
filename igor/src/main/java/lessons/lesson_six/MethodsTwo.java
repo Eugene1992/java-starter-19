4a0662b1a31c6fdcc992c195f81022dc59b9dd48
igorosha53 <igorosha@gmail.com>
2017-05-22 20:49:53 +0300

    in lesson six add ex.2

---
+++ igor/src/main/java/lessons/lesson_six/MethodsTwo.java
@@ -0,0 +1,16 @@
+package lessons.lesson_six;
+
+/**
+ * Created by User on 5/22/2017.
+ */
+public class MethodsTwo {
+    public static void main(String[] args) {
+        System.out.println(Literals(4,5));
+    }
+
+    public static int Literals(int a, int b) {
+        int c = a + b;
+        return c;
+    }
+
+}
