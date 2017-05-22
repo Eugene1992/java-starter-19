94406962a65679df37e4a097e7ea245cf6c1b4eb
Evgeniy <deyneko55@gmail.com>
2017-05-22 20:42:54 +0300

    Added cw06 examples.

--- igor/src/main/java/lessons/lesson_six/Methods.java
+++ igor/src/main/java/lessons/lesson_six/Methods.java
@@ -3,12 +3,14 @@ package lessons.lesson_six;
 /**
  * Created by User on 5/22/2017.
  */
-public class Metods {
+public class Methods {
     public static void main(String[] args) {
-        human("Igor " ,30);
+        human("Igor", 30);
+        human("Pavel", 4);
+
     }
 
     static void human(String name, int age) {
-        System.out.println(name + age);
+        System.out.println("My name is " + name + "! I'am " + age + " years old.");
     }
 }
