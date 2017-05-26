da3c58cbf997b90b3019a6eb92cc3b134e262d7a
Igor <igorosha@gmail.com>
2017-05-26 15:13:12 +0300

    testing class test in package myTest

--- igor/src/main/java/lessons/myTests/Test.java
+++ igor/src/main/java/lessons/myTests/Test.java
@@ -1,4 +1,5 @@
 package lessons.myTests;
+
 import java.util.Scanner;
 
 public class Test {
@@ -10,24 +11,23 @@ public class Test {
             System.out.println("Do you wanna one more beer? (" + alcLevel + ")");
             String answer = scan.next();
             if (answer.equals("+")) {
-                if (alcLevel == 4) {
+                if (alcLevel == 2) {
                     System.out.println("slow down");
-                } else if (alcLevel == 6) {
+                } else if (alcLevel == 3) {
                     System.out.println("chillout ");
-                } else if (alcLevel == 8) {
+                } else if (alcLevel == 4) {
                     System.out.println("on a horse ");
-                }
-                System.out.println("Cheers!");
-                alcLevel += 2;
-                if (alcLevel == 10) {
+                } else if (alcLevel == 5) {
                     System.out.println("Sorry, enough, bro!");
                     break;
                 }
+                System.out.println("Cheers!");
+                alcLevel ++;
             } else if (answer.equals("-")) {
-                System.out.println("Time to go home, dude!");
+                System.out.println("OK. Time to go home, dude!");
                 break;
             } else if (answer.equals("+-")) {
-                System.out.println("оприділися");
+                System.out.println("decide");
             } else {
                 System.out.println("I don't understand you...");
                 System.out.println("Choose minus (-) or pus(+) for your answer");
