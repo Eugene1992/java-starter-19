6bcff078eaef40bcff6b09447a4bdd84313c2b01
Evgeniy <deyneko55@gmail.com>
2017-05-31 21:14:00 +0300

    Added cw09 examples.

--- nikita/src/main/java/cw09/Final.java
+++ nikita/src/main/java/cw09/Final.java
@@ -7,21 +7,23 @@ import java.util.Scanner;
  */
 public class Final {
     public static void main(String[] args) {
-        int rand = (int) (Math.random() * 10);
+        int rand;
         System.out.println("Hello! I've just chose a random number. Try to guess. Good Luck!");
         Scanner scan = new Scanner(System.in);
+        int attempts = 0;
         while (true) {
+            rand = (int) (Math.random() * 10);
+            System.out.println("Enter your number, please: " + rand);
             int num = scan.nextInt();
-            int i = 0;
-            i++;
+            attempts++;
             if (num == rand) {
                 System.out.println("You're lucky!");
-                System.out.println("You guessed right from the " + i + " attempt");
+                System.out.println("You guessed right from the " + attempts + " attempts");
+                break;
             } else {
                 System.out.println("Sorry, you're wrong!");
             }
         }
-
     }
 }
 
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         int rand = (int) (Math.random() * 10); // [0, 9]
+        System.out.println("Welcome to out program!");
+        while (true) {
 
+        }
     }
 }
