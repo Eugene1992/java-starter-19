83a9ccf1b17d4d18e1b0cc9ba66fdbf2affa9ebd
igorosha53 <igorosha@gmail.com>
2017-05-31 20:56:19 +0300

    begin new class Random

---
+++ igor/src/main/java/lessons/myTests/IfElseTest.java
@@ -0,0 +1,12 @@
+package lessons.myTests;
+
+/**
+ * Created by User on 5/23/2017.
+ */
+public class IfElseTest {
+    public static void main(String[] args) {
+        if (true) {
+            System.out.println("Hello world!!!");
+        }
+    }
+}
